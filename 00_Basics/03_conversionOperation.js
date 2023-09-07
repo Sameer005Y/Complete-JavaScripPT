a3b4eaa24b6187a0776f919ab598e870f07418aa
Sameer Khan <sameer005saifi@gmail.com>
2023-09-07 10:28:06 +0000

    Conversion of DataTypes

---
+++ 00_Basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let value = "33abd";
+console.log(typeof value);
+
+
+let digit = Number(value);
+console.log(typeof digit);
+console.log(digit);
+
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// "true" <==> 1 // "False" <==> 0
