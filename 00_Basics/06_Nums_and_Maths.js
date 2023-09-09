d2189bf26f723f839d76fba192b4cd88fb06bf35
Sameer Khan <sameer005saifi@gmail.com>
2023-09-09 13:20:14 +0000

    Maths and Nums

---
+++ 00_Basics/06_Nums_and_Maths.js
@@ -0,0 +1,35 @@
+let num = 23;
+let num2 = new Number(100);
+
+// console.log(num)
+// console.log(num2)
+
+// console.log(num2.toString());
+// console.log(num2.toFixed(2))
+
+let num3 = 123.23;
+// console.log(num3.toPrecision(2));
+
+let digit = 1000000;
+// console.log(digit.toLocaleString('en-IN'));
+
+//**************************************MATH****************************************** */
+
+// console.log(Math);
+// console.log(Math.abs(-3));      //==>Convert any negative value to positive
+// console.log(Math.round(4.6))    //==>Convert any float value to its nearest numeric value
+// console.log(Math.ceil(4.3))     //==>Convert any float value to its next numeric value
+// console.log(Math.floor(4.9))    //==>Convert any float value to its previous value
+// console.log(Math.min(24,49,11,8));
+// console.log(Math.max(34,33,1,99));
+
+
+// console.log(Math.random());                     //==>It gives value between 0 or 1 (ex:-> 0.1223344 or 1.3464520).
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor((Math.random()*10)+1));  //==>To get random value starting from One.
+
+// //===>To get random value in range
+// const min = 20;
+// const max = 90;
+
+// console.log(Math.floor(Math.random()*(max-min))+min);
