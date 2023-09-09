33f096cc975dbf7347e07b561e8fe34771c1af7a
Sameer Khan <sameer005saifi@gmail.com>
2023-09-09 17:55:53 +0000

    Conversion Operations

---
+++ 00_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let value = "33abd";
+// console.log(typeof value);
+
+
+let digit = Number(value);
+// console.log(typeof digit);
+// console.log(digit);
+
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// "true" <==> 1 // "False" <==> 0
+
+
+// *********************** Operations *********************************
+
+let val = 33;
+let negVal = -val;
+// console.log(negVal);
+
+/*
+    console.log(2+2);
+    console.log(2-2);
+    console.log(2*2);
+    console.log(2**3);  ==> 2 to the power 3
+    console.log(2/2);
+    console.log(2%2);
+*/
+
+let str1 = "Sameer";
+let str2 = " Khan";
+let res = str1 + str2;
+// console.log(res);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( ( 3 + 4 ) * 5 % 3 );
