f18ddee68b2504d02ac26aa99cfb24b5fc4a742d
Sameer Khan <sameer005saifi@gmail.com>
2023-09-11 11:17:06 +0000

    IF ELSE Conditional Statements

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+// IF else
+// Comparison Operators -- < , > , <= , >= , == , != , === , !== 
+
+
+const temperature = 41;
+// if(temperature === 41){
+//     console.log("Less than 50");
+// }
+// else{
+//     console.log("Greater than 50")
+// }
+
+const score = 200;
+if(score > 100){
+    const power = "fly";
+    // console.log(`User power: ${power}`);
+}
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000;
+// if(balance < 500){
+//     console.log("Less than 500");
+// }else if(balance < 750){
+//     console.log("Less than 750");
+// }else if(balance < 900){
+//     console.log("Less than 900");
+// }else{
+//     console.log("Less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+// if(userLoggedIn && debitCard && 2 == 3){
+//     console.log("Allow to buy course");
+// }
+
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User Logged In");
+// }
