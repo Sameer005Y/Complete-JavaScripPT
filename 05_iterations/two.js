3a00523e4865b62f13c58af29c31b9bfc9398cb5
Sameer Khan <sameer005saifi@gmail.com>
2023-09-11 12:20:16 +0000

    While and Do-While Loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+
+let index =0;
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+let myArr = ["IronMan", "Captain America", "Thor"];
+let arr = 0;
+// while(arr < myArr.length){
+//     console.log(`Marvel Heroes are ${myArr[arr]}`);
+//     arr++;
+// }
+
+let score = 1;
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10)
